033c2bfd2b918fefcd6bca6f516f8d4a445bd691
DavidCB <70921381+DavidC031@users.noreply.github.com>
2022-11-23 23:00:20 -0500

    Add files via upload

---
+++ Close pairs divide and conquer/src/close/pairs/divide/and/conquer/Point.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package close.pairs.divide.and.conquer;
+
+/**
+ *
+ * @author crist
+ */
+public class Point {
+
+    int x;
+    int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point [x=" + x + ", y=" + y + "]";
+    }
+    
+    
+
+}
